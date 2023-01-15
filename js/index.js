25723b6fece26f58de94c7111006b58399a884d1
Sergey Kamensky <henry.128bit@gmail.com>
2023-01-15 14:47:54 +0300

    Add JS to slider

--- js/index.js
+++ js/index.js
@@ -1,8 +1,30 @@
 'use strict';
 
 const search = document.querySelector('.navbar__search-toggler'),
-      form = document.querySelector('.navbar__form');
+      form = document.querySelector('.navbar__form'),
+      sliderContainer = document.querySelector('.slider__container'),
+      sliderPreBtn = document.querySelector('.slider__pre-btn'),
+      sliderNxtBtn = document.querySelector('.slider__nxt-btn');
 
 search.addEventListener('click', () => {
     form.classList.toggle('navbar__form_active');
 });
+
+function scroll() {
+    let containerDimensions = sliderContainer.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    sliderNxtBtn.addEventListener('click', () => {
+        sliderContainer.scrollLeft += containerWidth;
+    });
+
+    sliderPreBtn.addEventListener('click', () => {
+        sliderContainer.scrollLeft -= containerWidth;
+    });
+}
+
+console.log(sliderContainer);
+console.log(sliderPreBtn);
+console.log(sliderNxtBtn);
+
+scroll();
